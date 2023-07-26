2525acfc655de8ae6f2b1e8cc461522a736d5db5
nixdonut <kanbhasin@gmail.com>
2023-07-27 00:27:02 +0530

    removed source choices

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Unit, SUBJECT_CHOICES, SOURCE_CHOICES
+from .models import Unit, SUBJECT_CHOICES
 
 
 class NewUserForm(UserCreationForm):
@@ -11,7 +11,7 @@ class NewUserForm(UserCreationForm):
 		model = User
 		fields = ("username", "email", "password1", "password2")
 
-	def save(self, commit=True):
+	def save(self, commit = True):
 		user = super(NewUserForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 		if commit:
@@ -27,6 +27,6 @@ class UploadUnitForm(forms.Form):
 
 	name = forms.CharField(max_length = 200)
 	subject = forms.ChoiceField(choices = SUBJECT_CHOICES)
-	source = forms.ChoiceField(choices = SOURCE_CHOICES)
+	source = forms.CharField(max_length = 200)
 	pdf = forms.FileField()
 	solutions = forms.FileField(required = False)
--- main/models.py
+++ main/models.py
@@ -8,15 +8,6 @@ SUBJECT_CHOICES = (
     ('Geometry', 'Geometry'),
     ('Number Theory', 'Number Theory'),
 )
-SOURCE_CHOICES = (
-    ('Euclidean Geometry in Mathematical Olympiads by Evan Chen', 'Euclidean Geometry in Mathematical Olympiads by Evan Chen'),
-    ('Modern Olympiad Number Theory by Aditya Khurmi', 'Modern Olympiad Number Theory by Aditya Khurmi'),
-    ('Stephan Wagner\'s Combinatorics Handout', 'Stephan Wagner\'s Combinatorics Handout'),
-    ('Olympiad Combinatorics by Pranav Sriram', 'Olympiad Combinatorics by Pranav Sriram'),
-    ('Secrets in Inequalities (Volume 1) by Pham Kim Hung', 'Secrets in Inequalities (Volume 1) by Pham Kim Hung'),
-    ('Secrets in Inequalities (Volume 2) by Pham Kim Hung', 'Secrets in Inequalities (Volume 2) by Pham Kim Hung'),
-    ('Evan Chen\'s Functional Equations Handout', 'Evan Chen\'s Functional Equations Handout'),
-)
 
 class Unit(models.Model):
 
@@ -26,19 +17,10 @@ class Unit(models.Model):
         ('Geometry', 'Geometry'),
         ('Number Theory', 'Number Theory'),
     )
-    SOURCE_CHOICES = (
-        ('Euclidean Geometry in Mathematical Olympiads by Evan Chen', 'Euclidean Geometry in Mathematical Olympiads by Evan Chen'),
-        ('Modern Olympiad Number Theory by Aditya Khurmi', 'Modern Olympiad Number Theory by Aditya Khurmi'),
-        ('Stephan Wagner\'s Combinatorics Handout', 'Stephan Wagner\'s Combinatorics Handout'),
-        ('Olympiad Combinatorics by Pranav Sriram', 'Olympiad Combinatorics by Pranav Sriram'),
-        ('Secrets in Inequalities (Volume 1) by Pham Kim Hung', 'Secrets in Inequalities (Volume 1) by Pham Kim Hung'),
-        ('Secrets in Inequalities (Volume 2) by Pham Kim Hung', 'Secrets in Inequalities (Volume 2) by Pham Kim Hung'),
-        ('Evan Chen\'s Functional Equations Handout', 'Evan Chen\'s Functional Equations Handout'),
-    )
     idx = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     name = models.CharField(max_length = 200)
     subject = models.CharField(max_length = 30, choices = SUBJECT_CHOICES)
-    source = models.CharField(max_length = 200, choices = SOURCE_CHOICES)
+    source = models.CharField(max_length = 200)
     pdf = models.FileField(upload_to = "units/")
     solutions = models.FileField(null = True, blank = True, upload_to = "solutions/")
     user = models.ForeignKey(User, on_delete = models.CASCADE)
